10545519d978428a26e5c83bbaea3cfd0a4724f9
JIVIIO <directorceo07@gmail.com>
2025-11-25 02:25:16 -0800

    Add config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'landing_builder');
+define('DB_USER', 'db_user');
+define('DB_PASS', 'db_pass');
+define('DB_CHARSET', 'utf8mb4');
+
+// основной домен сервиса
+define('MAIN_DOMAIN', 'mybuilder.com'); // поменяй на свой домен
